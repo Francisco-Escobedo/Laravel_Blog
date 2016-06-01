9e5e2ef241e071a732c1757fa5b487466e5386bc
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-06-01 11:05:41 -0500

    Random number generator that conducts php logic in routs and displays results in view (roll-dice.php).
    If guessed number is correct, good guess is displayed, otherwise, a try again message is output.

--- app/routes.php
+++ app/routes.php
@@ -25,3 +25,16 @@ Route::get('/portfolio', function()
 {
     return "<h1>This is my portfolio</h1>";
 });
+
+Route::get('/rolldice/{guess?}', function($guess='0')
+{
+    $randomNumber = mt_rand(1,6);
+    echo '<h1> Your guess: '. $guess. '<br>Random Number: '.$randomNumber.'</h1>';
+    if ($guess == $randomNumber){
+        echo '<h1> Good Guess! </h1>';
+    } else {
+        echo '<h1>Sorry! Try again.</h1>';
+    }
+    return View::make('roll-dice');
+
+});
